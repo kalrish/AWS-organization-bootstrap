e15787c1d4539d7d14e877e57fc1361c7c3c9386
David Joaquín Shourabi Porcel <david@djsp.eu>
2023-06-24 03:32:02 +0200

    REDO: Set up ESLint
    dependencies were installed with the following command:
    
    ```Shell
    corepack yarn add --dev -- eslint
    ```
    ```
    ```
    
    also setting for Visual Studio Code extension `dbaeumer.vscode-eslint`

---
+++ eslint.config.js
@@ -0,0 +1,10 @@
+// configuration for [ESLint][]
+// https://eslint.org/docs/latest/use/configure/configuration-files-new
+
+const configuration = [
+];
+
+export default configuration;
+
+
+// [ESLint]: https://eslint.org/
