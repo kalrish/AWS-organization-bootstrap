05125550f1829958afc3b9a16050283423f0e405
David Joaquín Shourabi Porcel <david@djsp.eu>
2023-06-24 03:46:50 +0200

    Enable recommended ESLint rules for JavaScript
    The only JavaScript for now is ESLint's own configuration file.
    
    Although most of this project/repository is / will be TypeScript, some
    code will be JavaScript:
    
    - ESLint new flat configuration file `eslint.config.js`
    - Jest configuration file `jest.config.js`
    
    The package `globals` has been installed with the following command:
    
    ```sh
    npm install --save-dev -- 'globals@^13.20.0'
    ```

--- eslint.config.js
+++ eslint.config.js
@@ -1,2 +1,16 @@
+import globals from "globals";
+import js from "@eslint/js";
+
 export default [
+	{
+		languageOptions: {
+			globals: {
+				// `nodeBuiltin` excludes CommonJS stuff
+				...globals.nodeBuiltin,
+			},
+		},
+		rules: {
+			...js.configs.recommended.rules,
+		},
+	},
 ];
