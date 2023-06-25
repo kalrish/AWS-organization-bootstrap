3a060bc7fd3929e892d8e776f1c2645ee984264e
David Joaquín Shourabi Porcel <david@djsp.eu>
2023-06-25 23:50:44 +0200

    src/sum.ts Jest test

---
+++ src/sum.test.ts
@@ -0,0 +1,24 @@
+import {
+	expect,
+	test,
+} from "@jest/globals";
+
+import sum from "./sum.js";
+
+test(
+	//"adds 1 + 2 to equal 3",
+	"",
+	(
+	) =>
+	{
+		expect(
+			sum(
+				1,
+				2,
+			),
+		)
+		.toBe(
+			3,
+		);
+	},
+);
---
+++ src/sum.ts
@@ -0,0 +1,10 @@
+function sum
+(
+	a: number,
+	b: number,
+)
+{
+	return a + b;
+}
+
+export default sum;
