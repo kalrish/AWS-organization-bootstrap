11c374c90df4cb76ea66f2ae1458636c813878ff
David Joaquín Shourabi Porcel <david@djsp.eu>
2023-06-25 18:02:28 +0200

    Add Visual Studio Code task for `markdownlint-cli2`

--- .markdownlint-cli2.mjs
+++ .markdownlint-cli2.mjs
@@ -2,6 +2,22 @@
 // https://github.com/DavidAnson/markdownlint-cli2#configuration
 
 const options = {
+	config: {
+
+		// Allow up to two consecutive blank lines
+		MD012: {
+			maximum: 2,
+		},
+		// https://github.com/DavidAnson/markdownlint/blob/main/doc/md012.md
+
+		// Allow lines of any length, because hard wrapping:
+		// - mixes up content and presentation and is therefore conceptually wrong;
+		// - requires rewrapping after most changes, which is cumbersome to do manually and otherwise requires appropriate tooling and configuration; and
+		// - results in bigger diffs than soft wrapping and semantic line breaks, at least with git's diff-highlight enabled.
+		MD013: false,
+		// https://github.com/DavidAnson/markdownlint/blob/main/doc/md013.md
+
+	},
 	globs: [
 		"**/*.md",
 	],
