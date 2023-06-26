be15b423c2c56dfca0d185fc47b0f4ae1b05be85
David Joaquín Shourabi Porcel <david@djsp.eu>
2023-06-26 14:50:50 +0200

    REDO: Enable recommended ESLint rules for JavaScript
    The only JavaScript for now is ESLint's own configuration file.
    
    Although most of this project/repository is / will be TypeScript, some
    code will be JavaScript:
    
    - ESLint new flat configuration file `eslint.config.js`
    - Jest configuration file `jest.config.js`
    
    dependencies installed with the following command:
    The package `globals` has been installed with the following command:
    
    ```Shell
    corepack yarn add --dev -- @eslint/js
    ```
    ```
    ```

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,43 @@
 // configuration for [ESLint][]
 // https://eslint.org/docs/latest/use/configure/configuration-files-new
 
+import js from "@eslint/js";
+
+const configurationForJavaScript = {
+	// The three patterns are the default:
+	//
+	// > By default, ESLint matches `**/*.js`, `**/*.cjs`, and `**/*.mjs`.
+	//
+	// However:
+	//
+	// > […] config objects that don’t specify `files` or `ignores` apply to all files that have been matched by any other configuration object, […].
+	//
+	// Without these patterns, this configuration object could end up applying to files it cannot handle.
+	files: [
+		"**/*.js",
+		"**/*.cjs",
+		"**/*.mjs",
+	],
+	ignores: [
+		// CommonJS loader for [Yarn Plug'n'Play][]
+		".pnp.cjs",
+		// ECMAScript module loader for [Yarn Plug'n'Play][]
+		".pnp.loader.mjs",
+		// managed by [Yarn][]
+		".yarn/**",
+	],
+	rules: {
+		...js.configs.recommended.rules,
+	},
+};
+
 const configuration = [
+	configurationForJavaScript,
 ];
 
 export default configuration;
 
 
 // [ESLint]: https://eslint.org/
+// [Yarn]: https://yarnpkg.com/
+// [Yarn Plug'n'Play]: https://yarnpkg.com/features/pnp
